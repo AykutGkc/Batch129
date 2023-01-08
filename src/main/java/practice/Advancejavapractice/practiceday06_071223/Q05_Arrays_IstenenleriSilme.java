86d4bbb200339ab2806a5aba9970756d29ee07b1
Aykut Gökce <aykutgokce2016@gmail.com>
2023-01-08 11:11:21 +0100

    v1

--- src/main/java/practice/Advancejavapractice/practiceday06_071223/Q05_Arrays_IstenenleriSilme.java
+++ src/main/java/practice/Advancejavapractice/practiceday06_071223/Q05_Arrays_IstenenleriSilme.java
@@ -1,16 +1,48 @@
 package practice.Advancejavapractice.practiceday06_071223;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Q05_Arrays_IstenenleriSilme {
     public static void main(String[] args) {
 
-//Bir integer arrayde verilen bir deger disinda elemanlar yeni bir array yazdiran method olusturunuz.
+
+        //Bir integer arrayde verilen bir deger disinda elemanlar yeni bir array yazdiran method olusturunuz.
+
+        int[]arr={1,2,2,3,4,5,5};
+
+        System.out.println(Arrays.toString(istenileniSil(arr)));
 
 
+    }//Main
+
+    public static int[] istenileniSil (int[] arr){
+        Scanner input=new Scanner(System.in);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Silmek istediginiz elemani giriniz.");
+        int silinecek=input.nextInt();
+
+        int length=0;
+
+        for (int w : arr) {
+            if (w!=silinecek){
+                length++;
+            }
+        }
+        System.out.println("length = " + length);
+
+        int[] arrSilinmis=new int[length];
+        int idx=0;
+
+        for (int w : arr) {
+            if (w!=silinecek){
+                arrSilinmis[idx]=w;
+                idx++;
+            }
+        }
+
+        return arrSilinmis;
+    }//Method
 
 
-
-
-
-
-    }
-}
+}//class
